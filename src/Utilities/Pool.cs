d793ca17195d61e05dadbb271cc7dc45cd35c00e
Ivan_Taturevich <ivan_taturevich@epam.com>
2017-09-16 01:17:25 +0300

    added MsBump, added coment to obkect pool class

--- src/Utilities/Pool.cs
+++ src/Utilities/Pool.cs
@@ -1,28 +1,59 @@
 using System;
 using System.Collections.Concurrent;
+using System.Linq;
 
 namespace Utilities
 {
+    /// <summary>
+    /// Simple implementation of object pool technic
+    /// </summary>
     public class Pool<T>
     {
         private Func<T> _objectFactory;
         private ConcurrentBag<T> _objects;
 
+        /// <summary>
+        /// Initialize object of <see cref="Pool"/> class
+        /// </summary>
+        /// <param name="objectFactory">creation object function</param>
         public Pool(Func<T> objectFactory)
         {
-            if (objectFactory == null) 
+            if (objectFactory == null)
             {
-                throw new ArgumentNullException("objectGenerator");
+                throw new ArgumentNullException("Object initializer factory cannot be null");
             }
 
-            _objects = new ConcurrentBag<T>();
             _objectFactory = objectFactory;
+            _objects = new ConcurrentBag<T>();
         }
 
+        /// <summary>
+        /// Initialize object of <see cref="Pool"/> class with initial set of values
+        /// </summary>
+        /// <param name="objectFactory">creation object function</param>
+        /// <param name="initialObjectNumber">number of initialized objects in pool</param>
+        public Pool(Func<T> objectFactory, int initialObjectNumber)
+        {
+            if (objectFactory == null)
+            {
+                throw new ArgumentNullException("Object initializer factory cannot be null");
+            }
+
+            _objectFactory = objectFactory;
+            _objects = new ConcurrentBag<T>();
+            for(int i = 0; i < initialObjectNumber; i++)
+            {
+                Put(objectFactory());
+            }
+        }
+
+        /// <summary>
+        /// Try get object from pool or create new one
+        /// </summary>
+        /// <returns>instance of type <see cref="T"/></returns>
         public T Get()
         {
-            T item;
-            if (_objects.TryTake(out item)) 
+            if (_objects.TryTake(out T item))
             {
                 return item;
             }
@@ -30,6 +61,10 @@ namespace Utilities
             return _objectFactory();
         }
 
+        /// <summary>
+        /// Put object into the Pool
+        /// </summary>
+        /// <param name="item">pooled object</param>
         public void Put(T item)
         {
             _objects.Add(item);
